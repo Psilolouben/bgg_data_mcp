d52d82d30cb2d18d7656be88d745b3a808ddd324
Markos Rigas <marky.rigas@gmail.com>
2025-11-12 00:45:47 +0200

    Add Bearer tokens to requests

--- lib/bgg_data.rb
+++ lib/bgg_data.rb
@@ -8,6 +8,7 @@ module BggData
 
   COLLECTION_BASE_URL = "https://www.boardgamegeek.com/xmlapi2/collection"
   BOARDGAME_BASE_URL = "https://www.boardgamegeek.com/xmlapi2/thing"
+  BEARER_TOKEN = 'Bearer c2d66922-196d-43e5-a1cb-56043f13eebd'
 
   class Error < StandardError; end
 
@@ -22,8 +23,8 @@ module BggData
   end
 
   def self.games_info(game_ids)
-    bgg_response = HTTParty.get(BOARDGAME_BASE_URL + "?id=#{game_ids.join(",")}&stats=1")
-    bgg_response.to_h["items"]["item"].map do |thing|
+    bgg_response = HTTParty.get(BOARDGAME_BASE_URL + "?id=#{game_ids.join(",")}&stats=1", headers: { Authorization: BEARER_TOKEN })
+    [bgg_response.to_h["items"]["item"]].flatten.map do |thing|
       {
         id: thing["id"],
         name: if thing["name"].is_a?(Array)
@@ -35,8 +36,15 @@ module BggData
               end,
         mechs: thing["link"].select { |t| t["type"] == "boardgamemechanic" }.map { |b| b["value"] },
         rank: thing["statistics"]["ratings"]["ranks"].any? ? thing["statistics"]["ratings"]["ranks"] : 888_888_888_888,
-        players: thing["poll"].first["results"].map { |x| { x["numplayers"] => recommended_players(x["result"]) } },
-        weight: thing["statistics"]["ratings"]["averageweight"]["value"].to_f
+        #players: thing["poll"].first["results"].map { |x| { x["numplayers"] => recommended_players(x["result"]) } },
+        best_players: thing['poll'].find{|x| x['name'] == 'suggested_numplayers'}['results'].map do |f|
+                  {
+                    f['numplayers'] => f['result'].find{|y| y['value'] == 'Best'}['numvotes']
+                  }
+                end,
+        weight: thing["statistics"]["ratings"]["averageweight"]["value"].to_f,
+        minimum_age: thing.dig('minage','value')
+
       }
     end
   end
@@ -61,18 +69,19 @@ module BggData
   end
 
   def self.collection(username, params = {})
-    collection_url = COLLECTION_BASE_URL + "?username=#{username}&own=1"
+    collection_url = COLLECTION_BASE_URL + "?username=#{username}&own=1&stats=1"
     collection_url += "&minbggrating=#{params[:minbggrating]}" if params[:minbggrating]
-    ::HTTParty.get(collection_url)
-    sleep(6)
-    bgg_response = ::HTTParty.get(collection_url)
+    #::HTTParty.get(collection_url, headers: { Authorization: BEARER_TOKEN })
+    sleep(10)
+    bgg_response = ::HTTParty.get(collection_url, headers: { Authorization: BEARER_TOKEN })
     return unless bgg_response
 
     bgg_response.to_h["items"]["item"]&.map do |game|
       {
         name: game["name"]["__content__"],
         bgg_id: game["objectid"],
-        plays: game["numplays"].to_i
+        plays: game["numplays"].to_i,
+        rating: game.dig('stats', 'rating', 'value')
       }
     end
   end
